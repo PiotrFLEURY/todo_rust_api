9f3f48d1c77e70aef1787b199f82a5aa8c6a1a5c
PiotrFLEURY <piotr.fleury@gmail.com>
2026-03-16 15:56:46 +0100

    feat(logs): add logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use logs::{debug, Logs};
 use std::env;
 
 use crate::{
@@ -12,13 +13,31 @@ mod presentation;
 
 #[tokio::main]
 async fn main() {
-    // Loading environment variables from .env file
     dotenv().ok();
+
+    Logs::new()
+        // Show log level color
+        .color(true)
+        // Filter log level
+        .level_from_env("LOG_LEVEL")
+        .unwrap_or_default()
+        .init();
+
+    debug!("Starting the Todo API server...");
+
+    debug!("Loading environment variables from .env file");
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let bind_address = env::var("BIND_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    debug!(
+        "Got environment variables: \nDATABASE_URL={} \nBIND_ADDRESS={} \nPORT={}",
+        database_url, bind_address, port
+    );
 
+    debug!("Creating database connection pool");
     let pool = create_pool(&database_url).await;
+
+    debug!("Creating repository and router");
     let repo = TodoRepositoryImpl::new(pool);
     let router = create_router(repo);
 
@@ -27,7 +46,10 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind(&server_address)
         .await
         .expect(format!("Unable to bind address {} on port {}", &bind_address, &port).as_str());
+    debug!("Server is running on {}", server_address);
     axum::serve(listener, router)
         .await
         .expect("Failed to start server");
+
+    debug!("Todo API server has stopped");
 }
