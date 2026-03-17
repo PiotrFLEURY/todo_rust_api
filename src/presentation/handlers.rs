5306b27e67890ee92d59b14d4f04e73f7ba8f4e2
Piotr FLEURY <piotr.fleury@gmail.com>
2026-03-17 10:11:11 +0100

    feat(logs): add logging (#2)

--- src/presentation/handlers.rs
+++ src/presentation/handlers.rs
@@ -3,25 +3,30 @@ use crate::domain::repository::TodoRepository;
 use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::{extract::State, Json};
+use logs::{debug, error, trace};
 
 pub async fn create_todo<R: TodoRepository>(
     State(repo): State<R>,
     Json(new_todo): Json<NewTodo>,
 ) -> Result<Json<Todo>, StatusCode> {
+    trace!("Creating new todo: {:?}", new_todo);
     let todo = repo
         .create_todo(&new_todo)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    debug!("Created todo: {:?}", todo);
     Ok(Json(todo))
 }
 
 pub async fn get_todos<R: TodoRepository>(
     State(repo): State<R>,
 ) -> Result<Json<Vec<Todo>>, StatusCode> {
+    trace!("Fetching all todos");
     let todos = repo
         .get_todos()
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    debug!("Fetched todos: {:?}", todos);
     Ok(Json(todos))
 }
 
@@ -29,13 +34,20 @@ pub async fn get_todo<R: TodoRepository>(
     State(repo): State<R>,
     Path(id): Path<i32>,
 ) -> Result<Json<Todo>, StatusCode> {
+    trace!("Fetching todo with id: {}", id);
     let todo = repo
         .get_todo(&id)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     match todo {
-        Some(todo) => Ok(Json(todo)),
-        None => Err(StatusCode::NOT_FOUND),
+        Some(todo) => {
+            debug!("Fetched todo: {:?}", todo);
+            Ok(Json(todo))
+        }
+        None => {
+            error!("Todo with id {} not found", id);
+            Err(StatusCode::NOT_FOUND)
+        }
     }
 }
 
@@ -44,13 +56,20 @@ pub async fn update_todo<R: TodoRepository>(
     Path(id): Path<i32>,
     Json(update_todo): Json<UpdateTodo>,
 ) -> Result<Json<Todo>, StatusCode> {
+    trace!("Updating todo with id: {} with data: {:?}", id, update_todo);
     let todo = repo
         .update_todo(&id, &update_todo)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     match todo {
-        Some(todo) => Ok(Json(todo)),
-        None => Err(StatusCode::NOT_FOUND),
+        Some(todo) => {
+            debug!("Updated todo: {:?}", todo);
+            Ok(Json(todo))
+        }
+        None => {
+            error!("Todo with id {} not found", id);
+            Err(StatusCode::NOT_FOUND)
+        }
     }
 }
 
@@ -58,8 +77,10 @@ pub async fn delete_todo<R: TodoRepository>(
     State(repo): State<R>,
     Path(id): Path<i32>,
 ) -> Result<StatusCode, StatusCode> {
+    trace!("Deleting todo with id: {}", id);
     repo.delete_todo(&id)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    debug!("Deleted todo with id: {}", id);
     Ok(StatusCode::NO_CONTENT)
 }
