35c9d5315c5876382e94915dcf5a3e75edfaa6fd
PiotrFLEURY <piotr.fleury@gmail.com>
2026-03-18 09:53:34 +0100

    tests(containers): create first test container

--- src/data/db.rs
+++ src/data/db.rs
@@ -4,13 +4,14 @@ use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 pub async fn connect_to_db(
     database_url: &str,
+    logging: bool,
 ) -> Result<DatabaseConnection, Box<dyn std::error::Error>> {
     let mut opt = ConnectOptions::new(database_url.to_string());
     opt.max_connections(100)
         .min_connections(5)
         .connect_timeout(Duration::from_secs(8))
         .acquire_timeout(Duration::from_secs(8))
-        .sqlx_logging(false); // set default Postgres schema
+        .sqlx_logging(logging);
 
     let connection = Database::connect(opt).await?;
     Ok(connection)
